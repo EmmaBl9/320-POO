79d27e6640c1f17df56af9a7d2e13310c769e83f
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2024-08-30 11:57:52 +0200

    snail

---
+++ Personnel/snail/snail/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace snail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////////////////////////
+            //   Décclaration variables   //
+            List<Snail> snails = new List<Snail>();
+
+            Snail toto = new Snail(10);
+            Snail titi = new Snail(13);
+
+            for (int i = 0; i < 10; i++)
+            {
+                snails.Add(new Snail(i));
+            }
+
+            ////////////////////////////////
+
+            Console.CursorVisible = false;
+
+            do
+            {
+                foreach (Snail snail in snails)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.SetCursorPosition(snail.X, snail.y);
+                    Console.WriteLine(snail.alive);
+                    snail.Move();
+                }
+                Thread.Sleep(50);
+                Console.Clear();
+            }
+            while (snails.First().pifle > 0);
+            foreach (Snail snail in snails)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(snail.X, snail.y);
+                Console.WriteLine(snail.dead);
+            }
+
+
+            //Game over
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(47, 15);
+            Thread.Sleep(1000);
+            Console.WriteLine("*Game Over*");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Personnel/snail/snail/Snail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snail
+{
+    internal class Snail
+    {
+        // Attributs
+        public int pifle = 50;                  // Point de vie
+        public int X = 0;                       // Position
+        public int y = 0;                       // Position
+        public String alive = "_@_ö";           // Escargot en vie
+        public String dead = "____";            // Escargot mort
+        public String name = "";
+
+        // Constructeur 
+        public Snail(int y)
+        {
+            this.y = y;
+            this.name = "Escargot" + y;
+        }
+
+        // Méthode
+        public void Move()
+        {
+            X++;
+            pifle--;
+        }
+
+
+    }
+}
