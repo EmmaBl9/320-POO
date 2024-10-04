3df3833c7b0c4083030ee5c4c0d0976a0b1d7b5b
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2024-10-04 10:26:29 +0200

    Create(Exercice3): Créer la classe box

---
+++ Personnel/Drones/Drones/Model/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones
+{
+    public class Box
+    {
+        private int id;
+        private int nombreMaxSmarties = 10;
+        private int nombreMinSmarties = 5;
+        private Color colorSmarties;
+
+        // Créer une instance random
+        public static Random Random = new Random();
+
+        public enum Color
+        {
+            RED, 
+            BLUE,
+            ORANGE,
+            YELLOW,
+            BROWN
+        }
+
+    }
+}
