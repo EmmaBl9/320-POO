9278e8d39d9ea3002c104ea8348099a725bf593b
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2024-10-04 12:12:00 +0200

    feat(320): Exercice 3 Point 4 :  en cours

--- Personnel/Drones/Drones/Dispatch.cs
+++ Personnel/Drones/Drones/Dispatch.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Drones
 {
-    internal class Dispatch
+    public class Dispatch : Building
     {
+        // Instancier la central de production
+        public Dispatch(int x, int y, int height, int width) : base(x, y, height, width)
+        {
+
+        }
     }
 }
